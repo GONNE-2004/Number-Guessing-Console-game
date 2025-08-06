01ef205eec81569fc9879a3618d0c6796f554c56
GONNE <gonne411@gmail.com>
2025-08-06 07:31:33 +0200

    added input validation for the guess in a function get_function and lastly added a play_agin input to restart or stop the game

--- main.py
+++ main.py
@@ -33,29 +33,60 @@ def compare_answer(user_guess, answer_guess, turns):
         print(f"You got it:{answer_guess}")
         return "congrats!"
 
+
+def get_guess(prompt : str):
+    "Ensures the user enters the correct data type again and again"
+    while True:
+        try:
+            guess = int(input(prompt))
+
+            if 1 <= guess <= 100: #enures the input range is btn 1-100
+                return guess
+            else:
+                print("Please enter a number between 1-100!")
+
+        except ValueError:
+            print("Input invalid, please enter a postive number.")
+
+
 def game():
     """contains logic which helps the game repeat again and again"""
-    print(logo)
-    print("Welcome the Number Guessing Game!")
-    print("Am thinking of a number between 1-100")
-    number = random.randint(1, 101)
-    print(number)
+    again = True
 
-    turns = difficulty() #initialised ii to it in order to choose btn easy and hard chances
+    while again:
+        print(logo)
+        print("Welcome the Number Guessing Game!")
+        print("Am thinking of a number between 1-100")
+        number = random.randint(1, 101)
+        print(number)
 
-    guess = 0
-    while guess != number:
-        print(f"You have:{turns} chances to guess the number!")
+        turns = difficulty() #initialised ii to it in order to choose btn easy and hard chances
 
-        guess = int(input("Guess the number: "))
+        guess = 0
+        while guess != number:
+            print(f"You have:{turns} chances to guess the number!")
 
-        turns = compare_answer(guess, number, turns) #initialised here but this time to compare in order to help with reducing the chances & comparing btn the 2 number
-        if turns == 0:
-            print("You have ran of out chances, you lose!")
-            print(f"The number was: {number}")
-            return
-        elif guess != number:
-            print("Guest again!")
+            guess = get_guess("Guess a number:  ")
+
+            turns = compare_answer(guess, number, turns) #initialised here but this time to compare in order to help with reducing the chances & comparing btn the 2 number
+            if turns == 0:
+                print("You have ran of out chances, you lose!")
+                print(f"The number was: {number}")
+                return
+            elif guess != number:
+                print("Guest again!")
+
+        play_again = input("Type 'yes' to restart the game or 'no' to stop: \n").lower().strip()
+        if play_again == "no":
+            again = False
+            print("Thanks for play, goodbye")
+        elif play_again == "yes":
+            print("\n" * 20)
+        else:
+            ("Please enter 'yes or no'")
+
+
+
 
 
 game()
